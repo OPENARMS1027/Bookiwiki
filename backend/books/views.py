596e9e6a3368a5fba72229c1b1ba75b0e6c74c4d
이진모 <lsoul1027@gmail.com>
2025-05-23 15:16:24 +0900

    feat: 도서 목록 조회 api 함수 기능 구현

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Book
+from .serializers import BookSerializer
+
+@api_view(['GET', 'POST'])
+def book_list(request):
+    if request.method == 'GET':
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+    
