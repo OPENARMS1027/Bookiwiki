cace1ee1d82765182568dc03e402fdb317bee25e
이진모 <lsoul1027@gmail.com>
2025-05-23 22:32:23 +0900

    feat: 스레드목록 기능 생성(재시도)

---
+++ frontend/src/views/ThreadsListView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>스레드 목록</h1>
+        <ThreadList />
+    </div>
+</template>
+
+<script setup>
+    import ThreadList from "@/components/ThreadList/ThreadList.vue";
+</script>
+
+<style scoped>
+
+</style>
