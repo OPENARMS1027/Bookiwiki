4a100aa3fd7e286cad9ab7787aa13073ff4b42ee
yena <rladpsk1117@gmail.com>
2025-05-24 21:05:01 +0900

    feat: 스레드 상세 페이지 책 정보 컴포넌트 작성

---
+++ frontend/src/components/ThreadDetail/BookInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="book-info-container">
+      <RouterLink :to="{ name: 'bookDetail', params: { bookId: book.id } }">
+        <img :src="book.cover" alt="book_cover_img"
+      /></RouterLink>
+      <div class="book-text">
+        <RouterLink :to="{ name: 'bookDetail', params: { bookId: book.id } }"
+          ><p class="book-title">{{ book.title }}</p>
+        </RouterLink>
+        <span class="book-author">{{ book.author }} | {{ book.pub_date }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  book: Object,
+})
+
+import { RouterLink } from 'vue-router'
+import '@/styles/ThreadDetail/BookInfo.css'
+</script>
+
+<style scoped></style>
