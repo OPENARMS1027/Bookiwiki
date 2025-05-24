e851acaf9dbc703544325d40d8c478dae17ffc3d
이진모 <lsoul1027@gmail.com>
2025-05-24 17:11:12 +0900

    feat: 책 상세 정보 기능 구현(back)

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("books/", views.book_list),
+    path("books/<int:book_id>/", views.book_detail),
     path("categories/", views.category_list),
     path("threads/", views.thread_list),
 ]
