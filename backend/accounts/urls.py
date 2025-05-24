2d3690da8b4c427ebf81a8a60ef324765e99d9c2
yena <rladpsk1117@gmail.com>
2025-05-25 04:59:35 +0900

    feat: 현재 로그인 한 유저 정보 조회 api 함수 작성

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:user_id>/", views.user_info)
+    path("<int:user_id>/", views.user_info),
+    path("me/", views.this_user),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
@@ -8,4 +9,10 @@ from .serializers import UserSerializer
 def user_info(request, user_id):
     user = get_object_or_404(get_user_model(), pk=user_id)
     serializer = UserSerializer(user)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def this_user(request): # 현재 로그인한 사용자 정보
+    serializer = UserSerializer(request.user)
     return Response(serializer.data)
