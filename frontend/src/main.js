d339d23684491dcf5e66f63c67b740cf4d131023
yena <rladpsk1117@gmail.com>
2025-05-25 03:40:59 +0900

    feat: Font Awesome 아이콘 라이브러리 적용

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,21 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createPinia } from 'pinia'
+import router from './router'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
-import App from './App.vue'
-import router from './router'
+library.add(fas, far, fab)
 
 const app = createApp(App)
 const pinia = createPinia()
-
 pinia.use(piniaPluginPersistedstate)
-// app.use(createPinia())
 app.use(pinia)
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
